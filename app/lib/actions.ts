869e1244306484cd0d952e1b21b38fd6f233bd89
taejinii <xowls177@gmail.com>
2023-10-28 23:44:07 +0900

    feat: chapter12(Server Action)

---
+++ app/lib/actions.ts
@@ -0,0 +1,62 @@
+"use server";
+
+import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+const InvoiceSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(["pending", "paid"]),
+  date: z.string(),
+});
+
+const CreateInvoice = InvoiceSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { amount, customerId, status } = CreateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  // 일반적으로 DB에 화폐 값을 센트 단위로 저장하여 자바스크립트 부동소수점 오류를 없애고 정확성을 높이기위해 센트 단위로 변환하여 저장한다.
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split("T")[0];
+
+  await sql`
+  INSERT INTO invoices (customer_id, amount, status, date)
+  VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+`;
+  // revalidatePath를 사용하면 특정 경로에 대해 캐시된 데이터를 온디맨드 방식으로 제거할수 있다. 즉 여기서 인보이스를 업데이트해서 /dashboard/invoices로 이동하면 해당 페이지는 캐시를 지웠기때문에 새로운데이터 요청을 보낼테고 데이터는 최신화된다.
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+const UpdateInvoice = InvoiceSchema.omit({ date: true });
+export async function updateInvoice(formData: FormData) {
+  const { id, customerId, amount, status } = UpdateInvoice.parse({
+    id: formData.get("id"),
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  await sql`
+      UPDATE invoices
+      SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+      WHERE id = ${id}
+    `;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+const DeleteInvoice = InvoiceSchema.pick({ id: true });
+export async function deleteInvoice(formData: FormData) {
+  const id = formData.get("id")?.toString();
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath("/dashboard/invoices");
+}
---
+++ 배운내용정리/chapter-12.md
@@ -0,0 +1,20 @@
+# _Next.js 14 App Router Guide / Chapter-11_
+
+[Next.js 14 guide chapter-12](https://nextjs.org/learn/dashboard-app/adding-search-and-pagination)
+
+# **Mutating Data**
+
+## _Server Action 이란?_
+
+**Server Action을 사용하면 데이터를 변경하기위해 API 엔드포인트를 만들 필요 없이 서버에서 실행되는 비동기 함수를 작성하고 클라이언트 또는 서버 컴포넌트에서 호출 가능하다.**
+
+**Next.js는 각 경로 브라우저에 일정 시간 동안 저장하는 클라이언트 측 라우터 캐시가 존재한다. 이 캐시는 prefetching과 함께 사용자가 서버에 대한 요청 횟수를 줄이면서 경로를 빠르게 탐색할수 있도록 한다.**
+
+**Server Action을 통해 데이터를 업데이트하고 업데이트된 페이지가 최신화 되길 원하면 위의 라우터 캐시를 지워야한다.
+이때 사용되는것 이 revalidatePath 이다**
+
+**revalidatePath(url) 를 사용하면 매개변수로 넘긴 url의 라우터 캐시는 삭제된다. 그러므로 해당 경로의 유효성이 다시 검사되고 서버에서 새로운 데이터를 가져올 수 있게 된다.**
+
+[revalidatePath](https://nextjs.org/docs/app/api-reference/functions/revalidatePath)
+
+[revalidateTag](https://nextjs.org/docs/app/api-reference/functions/revalidateTag)
