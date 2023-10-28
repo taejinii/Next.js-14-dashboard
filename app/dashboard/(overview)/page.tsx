723c15757b51b0c284f79dba3c06268b3c08bcbe
taejinii <xowls177@gmail.com>
2023-10-28 15:01:43 +0900

    feat: chapter09(Streaming)

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,19 +1,22 @@
-import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import { Suspense } from "react";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
+import { fetchCardData } from "@/app/lib/data";
 import {
-  fetchRevenue,
-  fetchLatestInvoices,
-  fetchCardData,
-} from "@/app/lib/data";
+  CardSkeleton,
+  CardsSkeleton,
+  LatestInvoicesSkeleton,
+  RevenueChartSkeleton,
+} from "@/app/ui/dashboard/skeletons";
+import Cards from "@/app/ui/dashboard/cards";
 
 export default async function Page() {
   // 아래와 같이 데이터를 요청하면 각 요청이 수행완료 될때까지 다음요청은 대기하게된다. 즉 페이지를 그리는데 오래걸린다. 이와 같은 현상을 waterfall 이라 부른다.
   // waterfall pattern 일때 데이터를 느리게 가져오는 요청이 있다면 해당요청 하나때문에 전체페이지가 block된다.
   // waterfall 을 사용해야할땐 예를들면 사용자의 정보를 가져오는 요청을 수행한후 해당 사용자의 정보를 가지고 친구목록을 가져오는 요청을하는경우 사용하면된다.
-  const revenue = await fetchRevenue();
-  const latestInvoices = await fetchLatestInvoices();
+  // const revenue = await fetchRevenue();
+  // const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfCustomers,
     numberOfInvoices,
@@ -26,18 +29,17 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Card title="Collected" value={totalPaidInvoices} type="collected" />
-        <Card title="Pending" value={totalPendingInvoices} type="pending" />
-        <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-        <Card
-          title="Total Customers"
-          value={numberOfCustomers}
-          type="customers"
-        />
+        <Suspense fallback={<CardsSkeleton />}>
+          <Cards />
+        </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue} />
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <RevenueChart />
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -14,26 +14,26 @@ const iconMap = {
   invoices: InboxIcon,
 };
 
-// export default async function Cards() {
-//   const {
-//     numberOfCustomers,
-//     numberOfInvoices,
-//     totalPaidInvoices,
-//     totalPendingInvoices,
-//   } = await fetchCardData();
-//   return (
-//     <>
-//       <Card title="Collected" value={totalPaidInvoices} type="collected" />
-//       <Card title="Pending" value={totalPendingInvoices} type="pending" />
-//       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
-//       <Card
-//         title="Total Customers"
-//         value={numberOfCustomers}
-//         type="customers"
-//       />
-//     </>
-//   );
-// }
+export default async function Cards() {
+  const {
+    numberOfCustomers,
+    numberOfInvoices,
+    totalPaidInvoices,
+    totalPendingInvoices,
+  } = await fetchCardData();
+  return (
+    <>
+      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      <Card title="Pending" value={totalPendingInvoices} type="pending" />
+      <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
+      <Card
+        title="Total Customers"
+        value={numberOfCustomers}
+        type="customers"
+      />
+    </>
+  );
+}
 
 export function Card({
   title,
