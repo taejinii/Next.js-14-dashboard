5db0cf17ea9ded77584a9316f35c8a7d1673e684
taejinii <xowls177@gmail.com>
2023-10-29 18:00:04 +0900

    update: matcher update

--- auth.config.ts
+++ auth.config.ts
@@ -7,14 +7,12 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log("auth", auth);
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        console.log("isLoggedIn", isLoggedIn);
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
