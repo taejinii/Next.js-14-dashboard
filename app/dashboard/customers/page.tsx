1dbd8aa823eb9bab8d9126d0138e9a7977c1f676
taejinii <xowls177@gmail.com>
2023-10-29 19:14:36 +0900

    feat: customes data fetch

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,13 +1,22 @@
 import CustomersTable from "@/app/ui/customers/table";
 import { Metadata } from "next";
+import { fetchCustomers, fetchFilteredCustomers } from "@/app/lib/data";
 
 export const metadata: Metadata = {
   title: "Customer",
 };
-export default function CustomerPage() {
+export default async function CustomerPage({
+  searchParams,
+}: {
+  searchParams: { query: string };
+}) {
+  const { query } = searchParams;
+  const customers = await fetchFilteredCustomers(query);
+  const customers2 = await fetchCustomers();
+  console.log("customers2", customers2);
   return (
     <div className="w-full">
-      <CustomersTable customers={[]} />
+      <CustomersTable customers={customers} />
     </div>
   );
 }
